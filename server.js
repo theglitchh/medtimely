7ce812a424648b1870255a438e30d209e2f3c158
thefallnn <thefallnn@pm.me>
2022-05-01 20:52:24 +0530

    JS: add npm && server.js for signup

---
+++ server.js
@@ -0,0 +1,37 @@
+//importing packages
+
+const express = require('express');
+const admin = require('firebase-admin');
+const bcrypt = require('bcrypt');
+const path =  require('path');
+
+let staticPath = path.join(__dirname);
+//initialising express.js
+
+const app = express();
+
+//routes
+//home route
+//middlewares
+app.use(express.static(staticPath));
+
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(staticPath,'index.html'));
+
+})
+//sign up
+app.get('/signup', (req,res)=>{
+    res.sendFile(path.join(staticPath,'signup.html'));
+})
+
+// 404 route
+app.get('/404',(req, res) => {
+    res.sendFile(path.join(staticPath,'404.html'));
+})
+app.use((req,res) => {
+    res.redirect('/404');
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000...");
+})
