1f624690cf1af7d4913f73f5b4c459302e05bf48
thefallnn <thefallnn@pm.me>
2022-05-03 23:39:50 +0530

    js:token:Add token and hide user details on browser console -medtimely:server:Implement User signup Protocol with firebase -signup.html:Connect form javascript file using script tag -js:form:Shift user details field to server.js -css:signup:Refactor loader image path
    TODO:
    -flush commented user details field

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path =  require('path');
 
+//firebase admin set
+let serviceAccount = require("./js/medtimely-01-firebase-adminsdk-qouyo-bbbcdb9215.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+ //after adding users in firebase
+
+ let db = admin.firestore();
+
+//declare static path
+
+
 let staticPath = path.join(__dirname);
+
 //initialising express.js
 
 const app = express();
@@ -14,14 +28,58 @@ const app = express();
 //home route
 //middlewares
 app.use(express.static(staticPath));
+app.use(express.json());
 
 app.get ('/', (req, res) => {
     res.sendFile(path.join(staticPath,'index.html'));
 
 })
-//sign up
+//sign up route
 app.get('/signup', (req,res)=>{
     res.sendFile(path.join(staticPath,'signup.html'));
+})
+
+app.post('/signup',(req, res) =>{
+    let{name ,email ,password , number, tac, notification} = req.body;
+
+    //form validation
+    if(name.length < 3){
+        return res.json({'alert': 'name must be 3 letters long'})
+    } else if(!email.length ){
+        return res.json({'alert':'Email is required'});
+    } else if(password.length < 8){
+        return res.json({'alert':'Password must be at least 8 characters long'});
+    }  else if(!number.length){
+        return res.json({'alert':'Number is required'});
+
+    } else if(!Number(number) || number.length < 10){
+        return res.json({'alert':'Number must be at least 10 digits long'});
+    } else if(!tac){
+        return res.json({'alert':'You must agree to our terms and conditions'});
+    } 
+    //store user in db
+    db.collection('users').doc(email).get()
+    .then(user => {
+        if(user.exists){
+            return res.json({'alert':'User already exists'});
+        } else{
+// now we will encrypt before storing it.
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    req.body.password = hash;
+                    db.collection('users').doc(email).set(req.body)
+                    .then(data => {
+                        res.json({
+                            name: req.body.name,
+                            email: req.body.email,
+                            seller: req.body.seller,
+                        })
+                    })
+                })
+            })
+
+        }
+    })
 })
 
 // 404 route
