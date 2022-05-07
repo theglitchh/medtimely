94ae067d9ce782b54cf4b18ed65ed59158e87f62
thefallnn <thefallnn@pm.me>
2022-05-07 21:40:39 +0530

    js:form: putting signup html code to if loop

--- js/form.js
+++ js/form.js
@@ -10,30 +10,31 @@ window.onload = () => {
 //select inputs
 const loader = document.querySelector('.loader');
 const submitBtn =  document.querySelector('.submit-btn');
-const name = document.querySelector('#name');
+const name = document.querySelector('#name') || null;
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const number = document.querySelector('#number');
-const tac = document.querySelector('#termscond');
-const notification = document.querySelector('label[for="notification"]');
+const number = document.querySelector('#number') || null;
+const tac = document.querySelector('#termscond') || null;
+const notification = document.querySelector('label[for="notification"]') || null;
 
 
 submitBtn.addEventListener('click',() =>{
-//    if(name.value.length < 3){
-//        showAlert('Name must be at least 3 characters long');
+   if(name != null ){ //signup page
+    if(name.value.length < 3){
+        showAlert('Name must be at least 3 characters long');
 
-//    } else if(!email.value.length ){
-//        showAlert('Email is required');
-//    }  else if(!number.value.length){
-//        showAlert('Number is required');
-//    } else if(!Number(number.value) || number.value.length < 10){
-//        showAlert('Number must be at least 10 digits long');
+   } else if(!email.value.length ){
+       showAlert('Email is required');
+   }  else if(!number.value.length){
+       showAlert('Number is required');
+   } else if(!Number(number.value) || number.value.length < 10){
+       showAlert('Number must be at least 10 digits long');
 
-//    } else if(password.value.length < 8){
-//        showAlert(('Password must be at least 8 characters long'));
-//    } else if(!tac.checked){
-//        showAlert('You must agree to our terms and conditions');
-//    } else {
+   } else if(password.value.length < 8){
+       showAlert(('Password must be at least 8 characters long'));
+   } else if(!tac.checked){
+       showAlert('You must agree to our terms and conditions');
+   } else {
        loader.style.display = 'block';
        sendData('/signup',{
            name: name.value,
@@ -44,8 +45,18 @@ submitBtn.addEventListener('click',() =>{
            notification: notification.checked,
            seller: false
        })
-  // }
+  }
+   }else{
+       //login page
+       if(!email.value.length || !password.value.length){
+           showAlert('fill all the inputs'); 
+   } else {
+
+   }
+}
+    
 })
+
 // send data  function
 const sendData=(path, data) =>{
     fetch(path,{
